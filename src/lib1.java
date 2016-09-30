c0263a6ec4a3f09499a7bf384e933d0408fc354b
81248397@qq.com <zhangxu@zhangxudeMBP.lan>
2016-09-30 13:50:01 +0800

    初版

--- src/lib1.java
+++ src/lib1.java
@@ -2,148 +2,197 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-public class lib1{
-	public static String change (String ss){
+public class lib1
+{
+	public static String change(String ss)
+	{
 		StringBuffer temps = new StringBuffer("");
-		int len=ss.length();
-		char [] vars=ss.toCharArray();
-		for(int i=0;i<len;i++){
-			if(vars[i]!=' ') temps.append(vars[i]);
+		int len = ss.length();
+		char[] vars = ss.toCharArray();
+		for (int i = 0; i < len; i++)
+		{
+			if (vars[i] != ' ')
+				temps.append(vars[i]);
 		}
-		String s=temps.toString();
+		String s = temps.toString();
 		return s;
 	}
-	public static boolean judge (String ss){
-		int len=ss.length();
-		char [] vars=ss.toCharArray();
-		for(int i=0;i+1<len;i++){
-			if(vars[i]=='*' || vars[i]=='+' || vars[i]=='-' || vars[i]=='^'){
-				if(vars[i+1]=='*' || vars[i+1]=='+' || vars[i+1]=='-' || vars[i+1]=='^'){
+
+	public static boolean judge(String ss)
+	{
+		int len = ss.length();
+		char[] vars = ss.toCharArray();
+		for (int i = 0; i + 1 < len; i++)
+		{
+			if (vars[i] == '*' || vars[i] == '+' || vars[i] == '-' || vars[i] == '^')
+			{
+				if (vars[i + 1] == '*' || vars[i + 1] == '+' || vars[i + 1] == '-' || vars[i + 1] == '^')
+				{
 					return false;
 				}
 			}
 		}
-		for(int i=0;i<len;){
-			if(vars[i]>='0' && vars[i]<='9') {
-				while(++i<len && (vars[i]>='0' && vars[i]<='9'));
-				if(i>=len) return true;
-				if(vars[i]>='a' && vars[i]<='z'){
-					while(++i<len && (vars[i]>='a' && vars[i]<='z'));
-					if(i>=len) return true;
-					if(vars[i]=='^'){
+		for (int i = 0; i < len;)
+		{
+			if (vars[i] >= '0' && vars[i] <= '9')
+			{
+				while (++i < len && (vars[i] >= '0' && vars[i] <= '9'))
+					;
+				if (i >= len)
+					return true;
+				if (vars[i] >= 'a' && vars[i] <= 'z')
+				{
+					while (++i < len && (vars[i] >= 'a' && vars[i] <= 'z'))
+						;
+					if (i >= len)
+						return true;
+					if (vars[i] == '^')
+					{
 						i++;
-						if(i>=len) return false;
-						if(vars[i]>='0' &&vars[i]<='9'){
-							while(++i<len && (vars[i]>='0'&&vars[i]<='9')||vars[i]==' ');
+						if (i >= len)
+							return false;
+						if (vars[i] >= '0' && vars[i] <= '9')
+						{
+							while (++i < len && (vars[i] >= '0' && vars[i] <= '9') || vars[i] == ' ')
+								;
 							continue;
 						}
-						else {
+						else
+						{
 							return false;
 						}
 					}
-					else if(vars[i]>='0' && vars[i]<='9') return false;
-					else continue;
+					else if (vars[i] >= '0' && vars[i] <= '9')
+						return false;
+					else
+						continue;
 				}
-				else continue;
+				else
+					continue;
 			}
-			if(vars[i]>='a' && vars[i]<='z'){
-				while(++i<len && (vars[i]>='a' && vars[i]<='z'));
-				if(i>=len) return true;
-				if(vars[i]=='^'){
+			if (vars[i] >= 'a' && vars[i] <= 'z')
+			{
+				while (++i < len && (vars[i] >= 'a' && vars[i] <= 'z'))
+					;
+				if (i >= len)
+					return true;
+				if (vars[i] == '^')
+				{
 					i++;
-					if(i>=len) return false;
-					if(vars[i]>='0' &&vars[i]<='9'){
-						if(i>=len) return true;
-						while(++i<len && (vars[i]>='0'&&vars[i]<='9'));
+					if (i >= len)
+						return false;
+					if (vars[i] >= '0' && vars[i] <= '9')
+					{
+						if (i >= len)
+							return true;
+						while (++i < len && (vars[i] >= '0' && vars[i] <= '9'))
+							;
 						continue;
 					}
-					else {
+					else
+					{
 						return false;
 					}
 				}
-				else if(vars[i]>='0' && vars[i]<='9') return false;
-				else continue;
+				else if (vars[i] >= '0' && vars[i] <= '9')
+					return false;
+				else
+					continue;
 			}
-			else i++;
+			else
+				i++;
 		}
 		return true;
 	}
-	public static section [] expression(String ss){
-		section [] temp=new section[1000];
-		for(int x=0;x<temp.length;x++){
-			temp[x]=new section();
+
+	public static section[] expression(String ss)
+	{
+		section[] temp = new section[10];
+		for (int x = 0; x < temp.length; x++)
+		{
+			temp[x] = new section();
 		}
-		int len=ss.length();
-		char [] vars=ss.toCharArray();
-		int cnt=0;
-		int j=0;
-		for(int i=0;i<len;){
+		int len = ss.length();
+		char[] vars = ss.toCharArray();
+		int cnt = 0;
+		int j = 0;
+		for (int i = 0; i < len;)
+		{
 			cnt++;
-			temp[cnt].val=1;
-			temp[cnt].cnt=0;
+			temp[cnt].val = 1;
+			temp[cnt].cnt = 0;
 			StringBuffer temps = new StringBuffer("");
-			while(vars[j]=='-' || vars[j]=='+') j++;
-			for(;j<len && vars[j]!='-' && vars[j]!='+';i=j+1){
-				if(vars[j]>='0' && vars[j]<='9'){
-					temp[cnt].val=vars[j]-'0';
-					while(++j<len && vars[j]>='0' && vars[j]<='9') {
-						temp[cnt].val*=10;
-						temp[cnt].val+=vars[j]-'0';
+			while (vars[j] == '-' || vars[j] == '+')
+				j++;
+			for (; j < len && vars[j] != '-' && vars[j] != '+'; i = j + 1)
+			{
+				if (vars[j] >= '0' && vars[j] <= '9')
+				{
+					temp[cnt].val = vars[j] - '0';
+					while (++j < len && vars[j] >= '0' && vars[j] <= '9')
+					{
+						temp[cnt].val *= 10;
+						temp[cnt].val += vars[j] - '0';
 					}
 				}
-				else if(vars[j]>='a' && vars[j]<='z'){
-					temps=new StringBuffer("");
-					temp[cnt].cnt+=1;
+				else if (vars[j] >= 'a' && vars[j] <= 'z')
+				{
+					temps = new StringBuffer("");
+					temp[cnt].cnt += 1;
 					temps.append(vars[j]);
-					while(++j<len && vars[j]>='a' && vars[j]<='z') temps.append(vars[j]);
-					String s=temps.toString();
-					temp[cnt].st[temp[cnt].cnt]=s;
-					temp[cnt].pow[temp[cnt].cnt]=1;
+					while (++j < len && vars[j] >= 'a' && vars[j] <= 'z')
+						temps.append(vars[j]);
+					String s = temps.toString();
+					temp[cnt].st[temp[cnt].cnt] = s;
+					temp[cnt].pow[temp[cnt].cnt] = 1;
 				}
-				else if(vars[j]=='^'){
-					temp[cnt].pow[temp[cnt].cnt]=0;
-					while(++j<len && vars[j]>='0' && vars[j]<='9') {
-						temp[cnt].pow[temp[cnt].cnt]*=10;
-						temp[cnt].pow[temp[cnt].cnt]+=vars[j]-'0';
+				else if (vars[j] == '^')
+				{
+					temp[cnt].pow[temp[cnt].cnt] = 0;
+					while (++j < len && vars[j] >= '0' && vars[j] <= '9')
+					{
+						temp[cnt].pow[temp[cnt].cnt] *= 10;
+						temp[cnt].pow[temp[cnt].cnt] += vars[j] - '0';
 					}
 				}
-				else if(vars[j]=='*') j++;
+				else if (vars[j] == '*')
+					j++;
 			}
 		}
 		return temp;
 	}
-	public static void main(String [] args) throws Exception
+
+	public static void main(String[] args) throws Exception
 	{
-//		BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-//		String s=br.readLine();
-//		System.out.println(s);
-//		section [] sec = new section[100];
-//		String regx="((((([a-zA-Z]|([0-9]+))\*){0,10})(([a-zA-Z]|([0-9]+)))[/+/-]){0,10})((([a-zA-Z]|([0-9]+))\*){0,10})(([a-zA-Z]|([0-9]+)))";
-//		s="x*y*2+35*z";
-//		s.matches("")
-		section[] a = expression("xx*y*2+35*z");
-		//System.out.println(change("  x *y "));
-		if(judge("x4")) System.out.println("Yes");
-		else System.out.println("No");
-		if(judge("x^")) System.out.println("Yes");
-		else System.out.println("No");
-		if(judge("3*y*x^4-10*e")) System.out.println("Yes");
-		else System.out.println("No");
-		int i=0;
-		while(a[++i].val>=1){
-			System.out.println(a[i].val);
-			for(int j=1;j<=a[i].cnt;j++){
-				System.out.print(a[i].st[j]+" ");
-				System.out.println(a[i].pow[j]);
-			}
+
+		section[] a = expression("xx*xx*y*2+35*z*z*xx*xx");
+		BufferedReader bufr= new BufferedReader(new InputStreamReader(System.in));
+		String command=bufr.readLine();
+		if(command.contains("!simplify "))
+		{
+			command=command.replace("!simplify ", "");
+			lib1_compute.simplify(command,a);
 		}
+		else if(command.contains("!d/d "))
+		{
+			command=command.replace("!d/d ", "");
+			lib1_compute.d(command,a);
+		}
+		else
+		{
+			System.out.println("illegal commad");
+		}
+
 	}
-	
+
 }
-class section{
-	boolean flag;//judge a section have a var or not
-	int val;//the xishu of the section and if the section have no var val means the value of the section
-	int cnt;//the number of the var in the section
-	String [] st = new String[100] ;//the var in the section
-	int [] pow=new int [100];//the pow of each var in the section
+
+class section
+{
+	boolean flag;// judge a section have a var or not
+	int val;// the xishu of the section and if the section have no var val means
+			// the value of the section
+	int cnt;// the number of the var in the section
+	String[] st = new String[100];// the var in the section
+	int[] pow = new int[100];// the pow of each var in the section
 }
--- src/lib1_compute.java
+++ src/lib1_compute.java
@@ -1,81 +1,148 @@
 import java.util.HashMap;
 
-public class lib1_compute {
-	public void simplify(String s,section [] sec)
+public class lib1_compute
+{
+	public static int contains(section sec, String s)
 	{
-		if(s=="")
+		for (int x = 1; x <= sec.cnt; x++)
 		{
-			String out ="";
-			for(section element:sec)
+			if (sec.st[x].equals(s))
+				return x;
+		}
+		return 0;
+	}
+
+	public static section reset(section sec)
+	{
+		section s = new section();
+		s.cnt = 0;
+		s.flag = sec.flag;
+		s.val = sec.val;
+		for (int x = 1; x <= sec.cnt; x++)
+		{
+			int count = contains(s, sec.st[x]);
+			if (count != 0)
+				s.pow[count] = s.pow[count] + 1;
+			else
 			{
-				
-				
-				out=out+element.val;
-				for(int x=0;x<element.cnt;x++)
-				{
-					out+="*";
-					out+=element.st[x];
-				}
-				//out+=element.+   需要定义每项后的符号
-					
+				s.cnt++;
+				s.st[s.cnt] = sec.st[x];
+				s.pow[s.cnt] = 1;
 			}
-			out=out.substring(0,out.length()-1);
-			System.out.println("the expression is:"+out);
-			return ;
+
 		}
-		String [] vars=s.split(",");
-		HashMap<String,Double> hm=new HashMap();
-		for(String var:vars)
+		return s;
+	}
+
+	public static void simplify(String s, section[] sec)
+	{
+		String[] vars = s.split(",");
+		HashMap<String, Double> hm = new HashMap();
+		for (String var : vars)
 		{
-			String[] lines=var.split("=");
-			hm.put(lines[0],Double.valueOf(lines[1]));
+			String[] lines = var.split("=");
+			hm.put(lines[0], Double.valueOf(lines[1]));
 		}
-		String out="";
-		double out_num=0;
-		boolean f1=false;//false代表 并非全部变量给出了初值
-		for(section se:sec)
+		String out = "";
+		double out_num = 0;
+		boolean f1 = false;// false代表 并非全部变量给出了初值
+		int i = 0;
+		while (sec[++i].val >= 1)
 		{
-			Double cnt=(Double)se.cnt;
-			String element="";
-			for(String var:se.st)
+			section se = sec[i];
+			String element = "";
+			int val = se.val;
+			for (int j = 1; j <= se.cnt; j++)
 			{
-				if(hm.containsKey(var))
+				String var = se.st[j];
+				if (hm.containsKey(var))
 				{
-					cnt*=hm.get(var);
+					val *= hm.get(var);
+
 				}
 				else
 				{
-					element+=("*"+var);
-					f1=true;
+					element += ("*" + var);
+					f1 = true;
 				}
 			}
-			//判断se后的符号，然后决定out_num是+=还是-=
-			//out=out+se.*+element.substring(0, element.length()-1);   *代表每个se后的加减号
+			if (i == 1)
+				out = out + val + element;
+			else
+				out = out + "+" + val + element;
+
+			// 判断se后的符号，然后决定out_num是+=还是-=
+			// out=out+se.*+element.substring(0, element.length()-1);
+			// *代表每个se后的加减号
 		}
-		if(f1=true)
+
+		if (f1 = true)// 有问题需要解决
 		{
-			System.out.println("resul:"+out_num);
+			System.out.println("result:" + out);
 		}
 		else
 		{
-			System.out.println(out);
+			String[] lines = out.split("+");
+			double result = 0;
+			for (String line : lines)
+			{
+				result += Double.valueOf(line);
+			}
+			System.out.println("result:" + result);
 		}
-		
-		return ;
-	}
-	public void d(String s,section [] sec)
-	{
-		
-	}
-	
-}
 
-/*
- *class section{
-	boolean flag;//judge a section have a var or not
-	int val;//the xishu of the section and if the section have no var val means the value of the section
-	int cnt;//the number of the var in the section
-	String [] st = new String[100] ;//the var in the section
-	int [] pow=new int [100];//the pow of each var in the section
-} 
- */
+		return;
+	}
+
+	public static void d(String v, section[] sec)
+	{
+		int i = 0;
+		while (sec[++i].val >= 1)
+		{
+			sec[i] = reset(sec[i]);
+		}
+		String out = "";
+		i = 0;
+
+		while (sec[++i].cnt >= 1)
+		{
+			section se = sec[i];
+			String element = "";
+			int val = se.val;
+			for (int j = 1; j <= se.cnt; j++)
+			{
+				if (se.st[j].equals(v))
+				{
+					val = val * (se.pow[j]);
+					if (se.pow[j] == 1)
+					{
+					}
+					else
+					{
+						for (int k = 1; k < se.pow[j]; k++)
+						{
+							element = element + "*" + se.st[j];
+						}
+					}
+				}
+				else
+				{
+					for (int k = 1; k <= se.pow[j]; k++)
+					{
+						element = element + "*" + se.st[j];
+					}
+				}
+
+				// 判断se后的符号，然后决定out_num是+=还是-=
+				// out=out+se.*+element.substring(0, element.length()-1);
+				// *代表每个se后的加减号
+			}
+			if (i == 1)
+				out = out + val + element;
+			else
+				out = out + "+" + val + element;
+		}
+		System.out.println("result:" + out);
+	}
+
+}
